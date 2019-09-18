5a27c1cf6e6ec91057fc88925d1bd91dbc8528cc
achayen <nithintsanil@gmail.com>
2019-09-19 02:24:03 +0530

    find largest number

---
+++ largest.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first num"))
+b=int(input("enter teh second num"))
+if(a>b):
+    print("a is greatest")
+elif(b>a):
+    print("b is largest")    
+else:
+    print("both are equal")    
